c82d7bbed8863ecf23b27310a4e12fc9015253e7
adityarevankarp <adityarevankar03@gmail.com>
2023-02-19 14:25:17 +0530

    SecondCommit

--- contracts/mapping.sol
+++ contracts/mapping.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.1;
 contract demo{
     mapping(uint => string) public roll_nos;
 
-    function setter(uint keys, string memory value) public {
-        roll_nos[keys] = value;
+    function setter(uint keys, string memory str) public {
+        roll_nos[keys] = str;
     }
 }
